bf229b614c7a199220898872103908e26524d72c
Leo_Alves <89089486+1LeoAlves@users.noreply.github.com>
2025-11-27 03:15:20 -0300

    rotas

--- FrontEnd/DjangoReactJs/src/services/api.js
+++ FrontEnd/DjangoReactJs/src/services/api.js
@@ -6,7 +6,7 @@ const API = axios.create({
 
 // injeta token automaticamente
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
+  const token = localStorage.getItem("access_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
